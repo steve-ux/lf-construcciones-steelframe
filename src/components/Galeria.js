b30862e6c4ad8ac9649793cbbadd3721b53ab05d
steve-ux <exe.universal@gmail.com>
2024-02-22 23:07:45 -0300

    Se agregaron fotos de clientes y se empezó la sección

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,15 +1,28 @@
+import Cetrogar from "../assets/1.png";
+import Libertad from "../assets/2.png";
+import Claro from "../assets/3.png";
+import YPF from "../assets/4.png";
+
 function Gallery() {
   return (
     <>
     <h2 className="titleGallery">Clientes</h2>
     <div className="gallery">
       <figure>
-        <img src="https://scontent.fmdz5-1.fna.fbcdn.net/v/t39.30808-6/261149425_4865939246783770_7800433290037400350_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeFBE-8AsOJSmVhQWfYIdOtUgWQqgIZI6waBZCqAhkjrBvENsTPCoiIjocJ02farWh_cAkfGRMvDC556vfcJlkEO&_nc_ohc=5H6GSis3iu0AX_5dTPn&_nc_ht=scontent.fmdz5-1.fna&oh=00_AfC_M_nkwdEBSqebDZySiSuKZwpyfvHUtFenCdcnA4LoqQ&oe=65DA6634" alt="Mountains" width="225" />
+        <img src={Cetrogar} alt="Mountains" width="230" />
         <figcaption>CETROGAR</figcaption>
       </figure>
       <figure>
-        <img src="https://pbs.twimg.com/media/EVucsd6XkAEDkY_.jpg:large" alt="Mountains" width="500" />
-        <figcaption>The Night</figcaption>
+        <img src={Libertad} alt="Mountains" width="230" />
+        <figcaption>HIPER<br></br>Libertad</figcaption>
+      </figure>
+      <figure>
+        <img src={Claro} alt="Mountains" width="230" />
+        <figcaption>SUCURSALES<br></br>CLARO</figcaption>
+      </figure>
+      <figure>
+        <img src={YPF} alt="Mountains" width="230" />
+        <figcaption>YPF<br></br>RICARDO ROJAS</figcaption>
       </figure>
     </div>
     </>
