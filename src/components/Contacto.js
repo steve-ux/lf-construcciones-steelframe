6b369a39cbee099f6fc8ccec0f9414729b52c7d2
steve-ux <exe.universal@gmail.com>
2024-03-18 21:59:14 -0300

    Se configuró el formulario y la página de Enviado

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -11,26 +11,31 @@ function Contacto() {
 				<small>Presupuesto sin cargo.</small>
 			</h2>
 		</div>
-		<form class="card-form">
+		<form class="card-form" action="https://formsubmit.co/luisfabianovalderrama09@gmail.com" method="post">
+		<label for="from">
+        </label>
 			<div class="input">
-				<input type="text" class="input-field" required/>
+				<input type="text" class="input-field" id="full_name" name="Nombre" required/>
 				<label class="input-label">Nombre, marca o proyecto</label>
 			</div>
       <div class="input">
-				<input type="number" class="input-field" required/>
+				<input type="number" class="input-field" id="tel" name="Teléfono" pattern="[0-9]{10}" required/>
 				<label class="input-label">Teléfono (Con código de área sin el 0 y el 15)</label>
 			</div>
 						<div class="input">
-				<input type="email" class="input-field" required/>
+				<input type="email" class="input-field" id="email" name="Correo" required/>
 				<label class="input-label">Email</label>
 			</div>
 						<div class="input">
-				<input type="text" class="input-field" required/>
+				<input type="text" class="input-field" id="message" name="Consulta" required/>
 				<label class="input-label">Mensaje</label>
 			</div>
 			<div class="action">
-				<button class="action-button">ENVIAR</button>
+				<button class="action-button" type="submit" value="Enviar">ENVIAR</button>
 			</div>
+			<input type="hidden" name="_next" value="https://lf-construcciones.com.ar/Enviado"/>
+            <input type="hidden" name="_subject" value="Nueva consulta desde la web LF Construcciones!"/>
+            <input type="hidden" name="_captcha" value="false" />
 		</form>
 		<div class="card-info">
 			<p>Al hacer click en enviar, acepta nuestros <a href="#">Términos y Condiciones.</a>Tu información está protegida.</p>
--- src/components/Enviado.js
+++ src/components/Enviado.js
@@ -1,9 +1,28 @@
-function Ejemplo() {
+import Navbar from "../components/Navbar";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+
+function Enviado() {
     return (
-    <div className="ejemplo">
-  
+      <>
+      <Navbar/>
+      <br/>
+      <br/>
+    <div className="enviado">
+      <div>
+        <h3 className="titleEnviado">
+          <span className="resaltados">¡Tu mensaje fue <span className="home-hero-underline">enviado!</span></span> A la brevedad vas a ser contactado. Muchas gracias.
+        </h3>
+        <div className="contenedorBtn">
+          <Link to="/">
+            <button className="btnEnviado">Seguir navegando</button>
+          </Link>
+         </div>
+        </div>
     </div>
+    <Footer/>
+    </>
     );
   }
   
-  export default Ejemplo;
+  export default Enviado;
