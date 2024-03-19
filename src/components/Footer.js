c8706690acaabc55926e8ab742f1876c597ebb45
steve-ux <exe.universal@gmail.com>
2024-03-18 21:11:38 -0300

    Se mejoró el footer y diseño, se dejó lista para hostear. Falta responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,6 @@ function Footer() {
     <div className="footer-padre">
 
       <div className="footer-hijo">
-      <img src={LF} alt="Logo de LF Construcciones" width="80"/>
-      <div>
-      <p className="text-footer">LF CONSTRUCCIONES Copyright ©{" "}
-          {new Date().getFullYear()} Todos los derechos reservados.</p>
-          <p className="text-footer">Córdoba, capital. Argentina.</p>
-      </div>
-      <div>
-      <a href="#"><img src={Arrow} alt="Logo de LF Construcciones" className="arrow" width="50"/></a>    
-      <p>SUBIR</p>
-      </div>
-      </div>
-      <div className="footer-hijo">
       <div className="footer-menu">
       <h4>MENÚ</h4>
       <p><a href="#Trabajos">TRABAJOS</a></p>
@@ -31,15 +19,25 @@ function Footer() {
       <p><a href="tel:3513028154" target="_blank" rel="noopener noreferrer">TELÉFONO: 3513028154</a></p>
       <p><a>HORARIO: DE 8 A 18 HS</a></p>
       </div>
-      <div className="footer-menu">
+      <div className="footer-menu3">
       <h4>UBICACIÓN</h4>
-      <p><a href="#Grilla">ALBERDI, CÓRDOBA. ARGENTINA</a></p>
-      <p><a href="tel:3513028154" target="_blank" rel="noopener noreferrer">CONSULTAR POR TRABAJOS EN</a></p>
-      <p><a>EL RESTO DEL PAÍS</a></p>
+      <p><a>ALBERDI, CÓRDOBA. ARGENTINA.</a></p>
+      <p><a href="tel:3513028154" target="_blank" rel="noopener noreferrer">CONSULTAR MÁS UBICACIONES.</a></p>
+      <p><a href="mailto:luisfabianovalderrama09@gmail.com" target="_blank" rel="noopener noreferrer">LUISFABIANOVALDERRAMA09@GMAIL.COM</a></p>
+      </div> 
       </div>
 
 
-      
+      <div className="footer-hijo">
+      <img src={LF} alt="Logo de LF Construcciones" width="80"/>
+      <div>
+      <p className="text-footer">LF CONSTRUCCIONES Copyright ©{" "}
+          {new Date().getFullYear()} Todos los derechos reservados.</p>
+      </div>
+      <div>
+      <a href="#"><img src={Arrow} alt="Logo de LF Construcciones" className="arrow" width="50"/></a>    
+      <p>SUBIR</p>
+      </div>
       </div>
       <hr></hr>
       <footer>
